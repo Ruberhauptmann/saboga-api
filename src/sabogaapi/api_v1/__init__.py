a69a08e22a879cc98ae46904d12e61dd04036036
Tjark Sievers <tjarksievers@icloud.com>
2024-05-16 15:36:03 +0200

    Add testing

--- src/sabogaapi/api_v1/__init__.py
+++ src/sabogaapi/api_v1/__init__.py
@@ -5,5 +5,11 @@ from sabogaapi.api_v1.routers import boardgames, plays
 
 api_v1 = FastAPI()
 
+
+@api_v1.get("/hello")
+def hello_world():
+    return "Hello World!"
+
+
 api_v1.include_router(boardgames.router)
 api_v1.include_router(plays.router)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,28 +1,11 @@
 import pytest
 from fastapi.testclient import TestClient
-from sqlmodel import Session, SQLModel, create_engine
-from sqlmodel.pool import StaticPool
 
-from sabogaapi.extensions import get_session
-from sabogaapi.main import app
-
-
-@pytest.fixture(name="session")
-def session_fixture():
-    engine = create_engine(
-        "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
-    )
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
-        yield session
+from sabogaapi import create_app
 
 
 @pytest.fixture(name="client")
-def client_fixture(session: Session):
-    def get_session_override():
-        return session
-
-    app.dependency_overrides[get_session] = get_session_override
+def client_fixture():
+    app = create_app()
     client = TestClient(app)
     yield client
-    app.dependency_overrides.clear()
