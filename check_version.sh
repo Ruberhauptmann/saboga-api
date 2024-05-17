1a957a326be02dd02b153b178e8ba946aa224670
Tjark Sievers <tjarksievers@icloud.com>
2024-05-17 19:41:17 +0200

    Check version on release branches

--- check_version.sh
+++ check_version.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-poetry config repositories.gitlab "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/pypi"
-pip install saboga-api --extra-index-url "https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.com/api/v4/projects/${CI_PROJECT_ID}/packages/pypi/simple"
-
 current_version=$(poetry version | awk '{print $2}')
 registry_version=$(pip show sabogaapi | grep "Version: " | awk '{print $2}')
 
