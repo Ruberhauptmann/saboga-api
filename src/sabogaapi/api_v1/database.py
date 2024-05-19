417c159b3fd817d88f73a027ab0733cf8481a856
Tjark Sievers <tjarksievers@icloud.com>
2024-05-19 13:31:13 +0200

    Add database connection

--- src/sabogaapi/api_v1/database.py
+++ src/sabogaapi/api_v1/database.py
@@ -1,3 +1,4 @@
+import os
 from typing import AsyncGenerator
 
 from beanie import init_beanie
@@ -12,7 +13,10 @@ async def get_user_db() -> AsyncGenerator[BeanieUserDatabase[User], None]:
 
 
 async def init_db() -> None:
-    client = AsyncIOMotorClient("mongodb://saboga-database:27017/boardgames")
+    db_password = os.getenv("MONGODB_API_USER")
+    client = AsyncIOMotorClient(
+        f"mongodb://api-user:{db_password}@saboga-database:27017/boardgames"
+    )
     await init_beanie(
         database=client.get_database(), document_models=[Boardgame, Play, User]
     )
