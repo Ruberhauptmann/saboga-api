5cd7629310909400e4dea05fb6163884752f0820
Tjark Sievers <tjarksievers@icloud.com>
2024-06-28 23:57:52 +0200

    Check bump version

--- check_version.sh
+++ check_version.sh
@@ -4,8 +4,10 @@ set -e
 current_version=$(poetry version | awk '{print $2}')
 registry_version=$(pip show sabogaapi | grep "Version: " | awk '{print $2}')
 
-if [[ "$current_version" == "$registry_version" ]];
+if [[ "$current_version" != "$registry_version" ]];
 then
-  echo "Version is not bumped!"
+  echo "Version is bumped!"
+  exit 0
+else
   exit 1
 fi
