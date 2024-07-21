52f38ed02ad14f81636d0774b6c61aedb9aa5dee
Tjark Sievers <tjarksievers@icloud.com>
2024-07-21 11:42:11 +0200

    Add scraper to regularly get new boardgame data

--- src/sabogaapi/api_v1/models.py
+++ src/sabogaapi/api_v1/models.py
@@ -1,32 +1,54 @@
 from datetime import datetime
 from typing import Annotated, List
 
-from beanie import Document, Indexed
+from beanie import Document, Indexed, ValidateOnSave, after_event
 from pydantic import BaseModel
 
 
 class Boardgame(Document):
-    bgg_id: Annotated[int, Indexed()]
-    name: str | None = None
-    bgg_geek_rating: float | None = None
-    bgg_average_rating: float | None = None
-    bgg_rank: List["RatingHistory"] = []
+    bgg_id: Annotated[int, Indexed(unique=True)]
+    name: str
+    bgg_rank: int | None
+    bgg_rank_change: int | None
+    bgg_geek_rating: float | None
+    bgg_geek_rating_change: float | None
+    bgg_average_rating: float | None
+    bgg_average_rating_change: float | None
+    bgg_rank_history: List["RankHistory"] = []
     last_data_sync: datetime | None = None
+
+    @after_event(ValidateOnSave)
+    def update_rank_history(self):
+        self.bgg_rank_history.append(
+            RankHistory(
+                date=datetime.now(),
+                bgg_rank=self.bgg_rank,
+                bgg_rank_change=self.bgg_rank_change,
+                bgg_geek_rating=self.bgg_geek_rating,
+                bgg_geek_rating_change=self.bgg_geek_rating_change,
+                bgg_average_rating=self.bgg_average_rating,
+                bgg_average_rating_change=self.bgg_average_rating_change,
+            )
+        )
+        self.last_data_sync = datetime.now()
 
     class Settings:
         name = "boardgames"
 
 
 class BoardgameSettings(Document):
-    last_bgg_ranking_scrape: datetime
-    last_bgg_scrape_page: int
-    max_bgg_scrape: int = 100
+    last_bgg_scrape: datetime
+    last_scraped_id: int
 
     class Settings:
         name = "boardgames.settings"
 
 
-class RatingHistory(BaseModel):
+class RankHistory(BaseModel):
     date: datetime
-    rank: int
-    change: int
+    bgg_rank: int | None
+    bgg_rank_change: int | None
+    bgg_geek_rating: float | None
+    bgg_geek_rating_change: float | None
+    bgg_average_rating: float | None
+    bgg_average_rating_change: float | None
--- src/sabogaapi/api_v1/routers/boardgames.py
+++ src/sabogaapi/api_v1/routers/boardgames.py
@@ -1,14 +1,16 @@
+import codecs
+import csv
 import math
-import re
-from datetime import datetime
-from typing import List
+import xml.etree.ElementTree as ElementTree
+from typing import Annotated, List
 
 import requests
 from beanie import PydanticObjectId
-from bs4 import BeautifulSoup
-from fastapi import APIRouter, HTTPException, Request, Response
+from fastapi import APIRouter, File, HTTPException, Request, Response, UploadFile
+from pydantic import BaseModel
+from pymongo.errors import DuplicateKeyError
 
-from sabogaapi.api_v1.models import Boardgame, BoardgameSettings, RatingHistory
+from sabogaapi.api_v1.models import Boardgame
 from sabogaapi.api_v1.schemas import BoardgamePublic
 
 router = APIRouter(
@@ -24,18 +26,19 @@ async def read_all_games(
     request: Request,
     page: int = 1,
     per_page: int = 100,
-    scrape: bool = True,
 ) -> List[Boardgame]:
-    if scrape is True:
-        await sync_ranking_data_from_bgg()
-
     total_count = await Boardgame.count()
     skip = (page - 1) * per_page
-    games = await Boardgame.find_all().skip(skip).limit(per_page).to_list()
+    games = (
+        await Boardgame.find({"bgg_rank": {"$type": "int"}})
+        .sort("+bgg_rank")
+        .skip(skip)
+        .limit(per_page)
+        .to_list()
+    )
 
     last_page = math.ceil(total_count / per_page)
     response.headers["link"] = ""
-
     if page > 1:
         response.headers[
             "link"
@@ -50,54 +53,6 @@ async def read_all_games(
     )
 
     return games
-
-
-async def sync_ranking_data_from_bgg() -> None:
-    boardgame_settings = await BoardgameSettings.find_all().first_or_none()
-    if boardgame_settings is None:
-        boardgame_settings = BoardgameSettings(
-            last_bgg_ranking_scrape=datetime.now(), last_bgg_scrape_page=1
-        )
-        await boardgame_settings.save()
-
-    if (datetime.now() - boardgame_settings.last_bgg_ranking_scrape).seconds > 10:
-        browse_page = requests.get(
-            f"https://boardgamegeek.com/browse/boardgame/page/{boardgame_settings.last_bgg_scrape_page}"
-        )
-        soup = BeautifulSoup(markup=browse_page.text, features="html.parser")
-        rows = soup.find_all("tr", id=re.compile("^row"))
-        for row in rows:
-            bgg_rank = int(
-                row.find(name="td", class_="collection_rank").get_text().strip()
-            )
-            bgg_id = int(
-                row.find(name="a", class_="primary")["href"].split("/")[2].strip()
-            )
-            game = await Boardgame.find(Boardgame.bgg_id == bgg_id).first_or_none()
-            if not game:
-                game = Boardgame(bgg_id=bgg_id)
-
-            if len(game.bgg_rank) == 0:
-                ranking = RatingHistory(date=datetime.now(), rank=bgg_rank, change=0)
-                game.bgg_rank.append(ranking)
-            else:
-                last_ranking = game.bgg_rank[-1]
-                if last_ranking.rank != bgg_rank:
-                    ranking = RatingHistory(
-                        date=datetime.now(),
-                        rank=bgg_rank,
-                        change=last_ranking.rank - bgg_rank,
-                    )
-                    game.bgg_rank.append(ranking)
-
-            await game.save()
-
-        boardgame_settings.last_bgg_ranking_scrape = datetime.now()
-        if boardgame_settings.last_bgg_scrape_page == boardgame_settings.max_bgg_scrape:
-            boardgame_settings.last_bgg_scrape_page = 0
-        else:
-            boardgame_settings.last_bgg_scrape_page += 1
-        await boardgame_settings.save()
 
 
 @router.get("/{game_id}", response_model=BoardgamePublic)
