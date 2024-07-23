a788dab0bbeffb2d4dfcbc680a1e17eb9d2e3c1a
Tjark Sievers <tjarksievers@icloud.com>
2024-07-23 09:42:55 +0200

    Try to fix scraper (#46)

--- src/sabogaapi/api_v1/scraper.py
+++ src/sabogaapi/api_v1/scraper.py
@@ -31,12 +31,13 @@ async def ascrape_full(step: int) -> None:
     sync_finished = False
     while sync_finished is False:
         if (datetime.now() - last_scrape_date).seconds < 10:
-            print("Waiting")
+            print("Waiting", flush=True)
             time.sleep(10)
 
         ids = list(range(last_scraped_id + 1, last_scraped_id + (step + 1)))
 
         try:
+            last_scrape_date = datetime.now()
             r = requests.get(
                 f"https://boardgamegeek.com/xmlapi2/thing?id={','.join(map(str, ids[0:40]))}"
             )
@@ -46,6 +47,7 @@ async def ascrape_full(step: int) -> None:
                 sync_finished = True
             else:
                 try:
+                    last_scrape_date = datetime.now()
                     r = requests.get(
                         f"https://boardgamegeek.com/xmlapi2/thing?id={','.join(map(str, ids))}&stats=1&type=boardgame"
                     )
@@ -101,7 +103,7 @@ async def ascrape_full(step: int) -> None:
                                 or boardgame.bgg_average_rating != average_rating
                                 or boardgame.bgg_geek_rating != geek_rating
                             ):
-                                print("Updating")
+                                print("Updating", flush=True)
                                 boardgame.bgg_rank_change = rank - boardgame.bgg_rank
                                 boardgame.bgg_geek_rating_change = round(
                                     geek_rating - boardgame.bgg_geek_rating, 5
@@ -113,13 +115,11 @@ async def ascrape_full(step: int) -> None:
                                 boardgame.bgg_geek_rating = geek_rating
                                 boardgame.bgg_average_rating = average_rating
                                 await boardgame.save()
-
-                    last_scrape_date = datetime.now()
                     last_scraped_id = bgg_id
                 except requests.exceptions.ChunkedEncodingError as e:
-                    print(f"Error: {e}, retrying.")
+                    print(f"Error: {e}, retrying.", flush=True)
         except requests.exceptions.ChunkedEncodingError as e:
-            print(f"Error: {e}, retrying.")
+            print(f"Error: {e}, retrying.", flush=True)
 
     boardgame_settings.last_bgg_scrape = datetime.now()
     boardgame_settings.last_scraped_id = last_scraped_id
@@ -132,14 +132,15 @@ async def ascrape_update(step: int) -> None:
     sync_finished = False
 
     last_scrape_date = datetime.now()
-    last_scraped_id = 0
+    last_scraped_rank = 0
 
     while sync_finished is False:
+        print(f"Last scraped before {last_scraped_rank}", flush=True)
         ids = (
             await Boardgame.find_all()
             .project(BoardgameBGGIDs)
-            .sort("+bgg_id")
-            .skip(last_scraped_id)
+            .sort("+bgg_rank")
+            .skip(last_scraped_rank)
             .limit(step)
             .to_list()
         )
@@ -147,19 +148,20 @@ async def ascrape_update(step: int) -> None:
         print(f"Scraping {ids}", flush=True)
 
         if (datetime.now() - last_scrape_date).seconds < 10:
-            print("Waiting")
+            print("Waiting", flush=True)
             time.sleep(10)
 
         if len(ids) == 0:
             sync_finished = True
         else:
             try:
+                last_scrape_date = datetime.now()
                 r = requests.get(
                     f"https://boardgamegeek.com/xmlapi2/thing?id={','.join(map(str, ids))}&stats=1&type=boardgame"
                 )
                 xml = ElementTree.fromstring(r.text)
                 items = xml.findall("item")
-                bgg_id = 0
+                rank = 0
                 for item in items:
                     bgg_id = int(item.get("id"))
                     ratings = item.find("statistics").find("ratings")
@@ -200,11 +202,10 @@ async def ascrape_update(step: int) -> None:
                         boardgame.bgg_geek_rating = geek_rating
                         boardgame.bgg_average_rating = average_rating
                         await boardgame.save()
-
-                last_scrape_date = datetime.now()
-                last_scraped_id = bgg_id
+                last_scraped_rank = rank
+                print(f"Last scraped after {last_scraped_rank}", flush=True)
             except requests.exceptions.ChunkedEncodingError as e:
-                print(f"Error: {e}, retrying.")
+                print(f"Error: {e}, retrying.", flush=True)
 
 
 def scrape() -> None:
