b649b41a9de9d1d7ed60af009ad68b03ef7e7445
Tjark Sievers <tjarksievers@icloud.com>
2025-03-20 17:47:33 +0100

    Return historical data for a single data

--- src/sabogaapi/api_v1/schemas.py
+++ src/sabogaapi/api_v1/schemas.py
@@ -1,18 +1,31 @@
 """Schemas for saboga API."""
 
+import datetime
+from typing import List
+
 from pydantic import BaseModel
+
+
+class RankHistory(BaseModel):
+    date: datetime.datetime
+    bgg_rank: int | None
+    bgg_geek_rating: float | None
+    bgg_average_rating: float | None
 
 
 class BaseBoardgame(BaseModel):
     bgg_id: int
     name: str
     bgg_rank: int
-    bgg_rank_change: int
     bgg_geek_rating: float
-    bgg_geek_rating_change: float
     bgg_average_rating: float
+
+
+class BoardgameComparison(BaseBoardgame):
+    bgg_rank_change: int
+    bgg_geek_rating_change: float
     bgg_average_rating_change: float
 
 
-class BoardgamePublic(BaseBoardgame):
-    pass
+class BoardgameWithHistoricalData(BaseBoardgame):
+    bgg_rank_history: List[RankHistory]
