5097816feb99d2bb4b655690999c983c31ce55f9
Tjark Sievers <tjarksievers@icloud.com>
2025-03-22 15:25:29 +0100

    Fix check version

--- check_version.sh
+++ check_version.sh
@@ -2,6 +2,8 @@
 set -euxo pipefail
 
 current_version=$(uv pip show sabogaapi | grep "Version: " | awk '{print $2}')
+
+docker pull ghcr.io/ruberhauptmann/saboga-api:latest
 registry_version=$(docker image inspect ghcr.io/ruberhauptmann/saboga-api:latest | jq -r '.[0].Config.Labels."org.opencontainers.image.version"')
 
 if [[ "$current_version" != "$registry_version" ]];
