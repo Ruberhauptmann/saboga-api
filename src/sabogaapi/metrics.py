d2f7c73c8a06197a704f03d7f7f77aa90f309c4a
Tjark Sievers <tjarksievers@icloud.com>
2025-03-23 21:51:26 +0100

    Add metric with unranked boardgames

---
+++ src/sabogaapi/metrics.py
@@ -0,0 +1,50 @@
+from typing import Any, Callable, Coroutine
+
+from prometheus_client import Gauge
+from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_fastapi_instrumentator.metrics import Info
+
+from sabogaapi.api_v1.models import Boardgame
+
+# Define Prometheus Gauge
+BOARDGAMES_WITHOUT_RANK = Gauge(
+    "boardgames_without_recent_rank",
+    "Number of board games where the latest bgg_rank is None",
+)
+
+
+def count_games_without_rank() -> Callable[[Info], Coroutine[Any, Any, None]]:
+    """
+    Function returning an instrumentation function that updates the Prometheus metric.
+    """
+
+    async def instrumentation(_: Info) -> None:
+        pipeline = [
+            {
+                "$addFields": {
+                    "latest_rank": {
+                        "$arrayElemAt": [
+                            {
+                                "$sortArray": {
+                                    "input": "$bgg_rank_history",
+                                    "sortBy": {"date": -1},
+                                }
+                            },
+                            0,
+                        ]
+                    }
+                }
+            },
+            {"$match": {"latest_rank.bgg_rank": None}},
+            {"$count": "count"},
+        ]
+
+        result = await Boardgame.aggregate(pipeline).to_list()
+        count = result[0]["count"] if result else 0
+
+        BOARDGAMES_WITHOUT_RANK.set(count)
+
+    return instrumentation
+
+
+instrumentator = Instrumentator().add(count_games_without_rank())
