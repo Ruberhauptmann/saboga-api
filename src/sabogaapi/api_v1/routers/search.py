ffdd5271f1ff3b50fbac47188ff0d6070d73cd12
Tjark Sievers <tjarksievers@icloud.com>
2025-03-24 21:25:17 +0100

    Add simple search function

---
+++ src/sabogaapi/api_v1/routers/search.py
@@ -0,0 +1,29 @@
+from typing import List
+
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+from sabogaapi.api_v1.models import Boardgame
+
+router = APIRouter(
+    prefix="/search",
+    tags=["Search"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+class SearchResult(BaseModel):
+    bgg_id: int
+    name: str
+
+
+@router.get("/", response_model=List[SearchResult])
+async def search(query: str, limit: int = 10) -> List[SearchResult]:
+    results = (
+        await Boardgame.find({"name": {"$regex": query, "$options": "i"}})
+        .project(SearchResult)
+        .limit(limit)
+        .to_list()
+    )
+
+    return results
