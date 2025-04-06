53c7287e0d7f92146397ce2e0dc9d5f2d17d6829
Tjark Sievers <tjarksievers@icloud.com>
2025-04-06 18:25:22 +0200

    Fix codspeed

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -3,24 +3,22 @@ import datetime
 
 import pytest
 
+from sabogaapi.api_v1.database import init_db
 from sabogaapi.api_v1.models import Boardgame
-
-from .conftest import seed_test_data, setup_database
 
 
 @pytest.mark.slow_integration_test
 def test_benchmark_get_top_ranked_boardgames(benchmark):
-    async def async_benchmark():
-        await setup_database()
-        await seed_test_data()
+    async def benchmark_get_top_ranked_boardgames():
+        await init_db()
         return await Boardgame.get_top_ranked_boardgames(
             compare_to=datetime.datetime.now(),
             page=1,
             page_size=50,
         )
 
-    def sync_wrapper():
-        return asyncio.run(async_benchmark())
+    def async_wrapper():
+        return asyncio.run(benchmark_get_top_ranked_boardgames())
 
-    result = benchmark(sync_wrapper)
+    result = benchmark(async_wrapper)
     assert result
