5b168511671eaf08e8e8c9ef4295b3835c9ea82c
Tjark Sievers <tjarksievers@icloud.com>
2025-06-17 08:15:15 +0200

    Fix codspeed

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -4,9 +4,10 @@ from fastapi.testclient import TestClient
 
 @pytest.mark.slow_integration_test
 def test_read_boardgames(app, benchmark) -> None:
-    def benchmark_function():
-        return client.get("v1/boardgames/rank-history")
-
     with TestClient(app) as client:
+
+        def benchmark_function():
+            return client.get("v1/boardgames/rank-history")
+
         response = benchmark(benchmark_function)
     assert response.status_code == 200
