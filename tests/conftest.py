f2721e4a9db3a81d8231e771cd96ab9d64769c1d
Tjark Sievers <tjarksievers@icloud.com>
2025-08-14 08:12:42 +0200

    Fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
+import time
 from contextlib import asynccontextmanager
 
+import docker
 import pytest
 from beanie import init_beanie
 from fastapi import FastAPI
@@ -25,7 +27,45 @@ async def lifespan(app: FastAPI):
     yield
 
 
+@pytest.fixture(scope="session")
+def docker_client():
+    """Return a Docker client"""
+    return docker.from_env()
+
+
+@pytest.fixture
+def mongodb_container(docker_client):
+    container = docker_client.containers.run(
+        "docker.io/mongo:8.0-noble",
+        detach=True,
+        name="pytest-mongodb",
+        ports={"27017/tcp": 27017},
+        environment={
+            "MONGO_INITDB_ROOT_USERNAME": "mongoadmin",
+            "MONGO_INITDB_ROOT_PASSWORD": "password",
+            "MONGO_INITDB_DATABASE": "boardgames",
+        },
+    )
+    yield container
+
+    container.remove(force=True)
+
+
+@pytest.fixture
+def mongodb_host(mongodb_container):
+    while True:
+        mongodb_container.reload()
+        try:
+            networks = list(
+                mongodb_container.attrs["NetworkSettings"]["Networks"].values()
+            )
+            addr = networks[0]["IPAddress"]
+            return addr
+        except KeyError:
+            time.sleep(0.5)
+
+
 @pytest.fixture()
-def app():
+def app(mongodb_host):
     app = create_app(lifespan=lifespan)
     yield app
