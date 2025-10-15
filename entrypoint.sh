539df8e2c2458f21da12bcc02805ac8e466ae9c6
Tjark Sievers <tjarksievers@icloud.com>
2025-10-16 00:18:56 +0200

    Fix dockerfile (#178)

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Running Alembic migrations..."
+alembic upgrade head
+
+echo "Starting FastAPI..."
+fastapi run /app/src/sabogaapi/main.py --root-path /api --host 0.0.0.0 --proxy-headers --port 8000
